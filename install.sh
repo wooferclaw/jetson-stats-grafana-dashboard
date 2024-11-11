fa8ab1d523db3f0a0414d5822af53be39e0b5265
Grigory Frantsuzov <grigoriy.frantsuzov@quantori.com>
2024-11-11 14:56:29 +0300

    updated prereqs

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ sudo cp jetson_stats_prometheus_collector.py /usr/local/bin/
 # Install service for the current user
 mkdir -p ~/.config/systemd/user
 cp jetson_stats_prometheus_collector.service ~/.config/systemd/user/
-systemctl --user daemon-reload
-systemctl --user start jetson_stats_prometheus_collector
-systemctl --user status jetson_stats_prometheus_collector
+sudo systemctl daemon-reload
+sudo systemctl start jetson_stats_prometheus_collector
+sudo systemctl status jetson_stats_prometheus_collector
