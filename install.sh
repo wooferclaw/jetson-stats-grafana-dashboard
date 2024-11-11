8f70675dd644fbc8af3c424ba9d429461acec22c
Grigory Frantsuzov <grigoriy.frantsuzov@quantori.com>
2024-11-11 15:00:02 +0300

    fixed installation

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
 pip install prometheus-client
 sudo cp jetson_stats_prometheus_collector.py /usr/local/bin/
 
-# Install service for the current user
-mkdir -p ~/.config/systemd/user
-cp jetson_stats_prometheus_collector.service ~/.config/systemd/user/
+sudo cp jetson_stats_prometheus_collector.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl start jetson_stats_prometheus_collector
 sudo systemctl status jetson_stats_prometheus_collector
